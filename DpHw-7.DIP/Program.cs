32d06209fb207fcd5ed2fc0ab8f8052120f143c8
Данил Шипилов <danil.shipilov.99@gmail.com>
2019-07-11 23:56:45 +0600

    DpHw-7

---
+++ DpHw-7.DIP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpHw_7.DIP
+{
+    // Интерфейс отправки сообщения
+    public interface IMessage
+    {
+        void SendMessage();
+    }
+
+    // Класс отправки по Email
+    public class Email : IMessage
+    {
+        public string ToAddress { get; set; }
+        public string Message { get; set; }
+        public void SendMessage()
+        {
+            Console.WriteLine("Отправлено письмо на адрес {0}, с текстом: {1}", ToAddress, Message);
+        }
+    }
+
+    // Класс отправки по Sms
+    public class SMS : IMessage
+    {
+        public string ToPhoneNumber { get; set; }
+        public string Message { get; set; }
+        public void SendMessage()
+        {
+            Console.WriteLine("Отправлено смс на номер {0}, с текстом: {1}", ToPhoneNumber, Message);
+        }
+    }
+
+    // Выполняет действия в зависимости от того, какой способ отправки выбран
+    public class MessageService
+    {
+        public IMessage Sender { get; set; }
+
+        public MessageService(IMessage sender)
+        {
+            this.Sender = sender;
+        }
+        public void Send()
+        {
+            Sender.SendMessage();
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageService notification = new MessageService(new Email { ToAddress = "danil@gmail.com", Message = "Privet" });
+            notification.Send();
+            notification.Sender = new SMS { ToPhoneNumber = "+77011230322", Message = "Privet" };
+            notification.Send();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DpHw-7.ISP/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpHw_7.ISP
+{
+    // Интерфейс для звонка
+    interface ICall
+    {
+        void Call();
+    }
+    // Интерфейс для фото
+    interface IPhoto
+    {
+        void TakePhoto();
+    }
+
+    // Камера может делать снимки, но не предназначена для звонков.
+    // Поэтому реализует только интерфейс IPhoto
+    class Camera : IPhoto
+    {
+        public void TakePhoto()
+        {
+            Console.WriteLine("Фотографируем с помощью фотокамеры");
+        }
+    }
+
+    // Похожая логика и с домашним телефоном, только наоборот.
+    // Звонить можно, а снимать нет
+    class HomePhone : ICall
+    {
+        public void Call()
+        {
+            Console.WriteLine("Звоним по домашнему телефону");
+        }
+    }
+
+    // Но смартфон в свою очередь может принимать звонки иделать снимки
+    // Поэтому реализует оба интерфейса
+    class MobilePhone : ICall, IPhoto
+    {
+        public void Call()
+        {
+            Console.WriteLine("Звоним по смартфону");
+        }
+        public void TakePhoto()
+        {
+            Console.WriteLine("Фотографируем с помощью смартфона");
+        }
+    }
+    
+    // Таким образом фотограф не сможет сделать снимок, например, с домашнего телефона 
+    // так как он для этого не предназначен и не реализует нужный интерфейс
+    class Photograph
+    {
+        public void TakePhoto(IPhoto photoMaker)
+        {
+            photoMaker.TakePhoto();
+        }
+
+        public void Call(ICall photoMaker)
+        {
+            photoMaker.Call();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Photograph photograph = new Photograph();
+            MobilePhone mobilePhone = new MobilePhone();
+            HomePhone homePhone = new HomePhone();
+            Camera camera = new Camera();
+
+            photograph.TakePhoto(mobilePhone);
+            photograph.TakePhoto(camera);
+
+            photograph.Call(mobilePhone);
+            photograph.Call(homePhone);
+        }
+    }
+}
